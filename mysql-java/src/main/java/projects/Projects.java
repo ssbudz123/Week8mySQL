93e9870413e7017d79095455fbdf05d6abeb2ab4
Shawn <ssbudz123@gmail.com>
2022-08-20 16:22:38 -0400

    Add files via upload

---
+++ mysql-java/src/main/java/projects/Projects.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package projects;
+
+import java.sql.Connection;
+
+import projects.dao.DbConnection;
+
+/**
+ * @author shawnbudzinski
+ *
+ */
+public class Projects {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ DbConnection.getConnection();
+	}
+
+}
